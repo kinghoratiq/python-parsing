961af901214ce595014b10af40820dec32a677c2
kinghoratiq <35841726+kinghoratiq@users.noreply.github.com>
2018-06-08 11:52:05 +0800

    Add files via upload
    added parsing for answers & metadata of answers (answer, author, upvotes)

--- web-parser.py
+++ web-parser.py
@@ -8,22 +8,32 @@ data = {}
 question = {}
 data['question'] = question
 
-dirtyQTitle = soup.find('a', attrs={'class':'question-hyperlink'})
-cleanQTitle = dirtyQTitle.text
-question['title'] = cleanQTitle
+dirtyTitle = soup.find('a', attrs={'class':'question-hyperlink'})
+cleanTitle = dirtyTitle.text
+question['title'] = cleanTitle
 
 for words in soup.find_all('div', attrs={'class':'postcell'}):
-	cleanQBody = (words.find('div', attrs={'class':'post-text'}).text)
-	userDetails = (words.find('div', attrs={'class':'user-details'}))
-	asker = userDetails.find('a').text
-question['body'] = cleanQBody
-question['asker'] = asker
+	qCleanBody = words.find('div', attrs={'class':'post-text'}).text
+	qUserDetails = words.find('div', attrs={'class':'user-details'})
+	qAuthors = []
+	qAuthors.append(qUserDetails.find('a').text)
+question['body'] = qCleanBody
+question['authors'] = qAuthors
 
-upvotes = soup.find('span',attrs={'itemprop':'upvoteCount'}).text
-question['upvotes'] = upvotes
-print(question['body'])
+qUpvotes = soup.find('span', attrs={'itemprop':'upvoteCount'}).text
+question['upvotes'] = qUpvotes
+#print(question)
 
 answers = []
-for words in soup.find_all('div', attrs={'class':'answer'}):
-	for moreWords in words.find_all('p'):
-		answers.append(moreWords.text)
+data['answers'] = answers
+for stuff in soup.find_all(id='answers'):
+	answer = {}
+	for words in soup.find_all('div', attrs={'class':'answercell'}):
+		answer['body'] = words.find('div', attrs={'class':'post-text'}).text
+		aUserDetails = words.find('div', attrs={'class':'user-details'})
+		aAuthors = []
+		aAuthors.append(aUserDetails.find('a').text)
+		answer['authors'] = aAuthors
+	for votes in stuff.find_all('span', attrs={'itemprop':'upvoteCount'}):
+		answer['upvotes'] = votes.text
+	answers.append(answer)
