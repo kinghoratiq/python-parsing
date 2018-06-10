7da5066c22006f4c9e595cd77e44cbe540b1b3f3
kinghoratiq <35841726+kinghoratiq@users.noreply.github.com>
2018-06-10 17:09:00 +0800

    Add files via upload
    fixed comments in question, fixed extraction of answers and comments

--- web-parser.py
+++ web-parser.py
@@ -7,6 +7,7 @@ soup = BeautifulSoup(webpage, 'html.parser')
 
 data = {}
 question = {}
+data['question'] = question
 qComments = []
 
 title = soup.find('a', attrs={'class':'question-hyperlink'}).text
@@ -14,9 +15,10 @@ question['title'] = title
 
 for words in soup.find_all('div', attrs={'class':'postcell'}):
 	qBody = words.find('div', attrs={'class':'post-text'}).text
-	qUserDetails = words.find('div', attrs={'class':'user-details'})
 	qAuthors = []
-	qAuthors.append(qUserDetails.find('a').text)
+	for users in words.find_all('div', attrs={'class':'user-details'}):
+		qAuthors.append(users.find('a').text)
+	qUpvotes = words.find_previous_sibling().find('span', attrs={'itemprop':'upvoteCount'}).text
 	for comments in words.find_next_sibling().find_all('li', attrs={'class':'comment'}):
 		qComment = {}
 		qComment['comment'] = comments.find('span',attrs={'class':'comment-copy'}).text
@@ -26,25 +28,31 @@ for words in soup.find_all('div', attrs={'class':'postcell'}):
 
 question['body'] = qBody
 question['authors'] = qAuthors
-qUpvotes = soup.find('span', attrs={'itemprop':'upvoteCount'}).text
 question['upvotes'] = qUpvotes
 question['comments'] = qComments
 
 answers = []
 data['answers'] = answers
 answer = {}
-aComments = []
-answer['comments'] = aComments
+
 for words in soup.find_all('div', attrs={'class':'answercell'}):
-	answer['body'] = words.find('div', attrs={'class':'post-text'}).text
-	aUserDetails = words.find('div', attrs={'class':'user-details'})
+	answer = {}
+	aComments = []
+	aBody = words.find('div', attrs={'class':'post-text'}).text
 	aAuthors = []
-	aAuthors.append(aUserDetails.find('a').text)
-	answer['authors'] = aAuthors
+	for users in words.find_all('div', attrs={'class':'user-details'}):
+		aAuthors.append(users.text)
+	aUpvotes = words.find_previous_sibling().find('span', attrs={'itemprop':'upvoteCount'}).text
 	for comments in words.find_next_sibling().find_all('li', attrs={'class':'comment'}):
 		aComment = {}
-		aComment['comment'] = comments.find('span',attrs={'class':'comment-copy'}).text
+		aComment['comment'] = comments.find('span', attrs={'class':'comment-copy'}).text
 		aComment['author'] = comments.find('a', attrs={'class':'comment-user'}).text
 		aComment['upvotes'] = comments.find('div', attrs={'class':'comment-score'}).text
 		aComments.append(aComment)
+	answer['body'] = aBody
+	answer['authors'] = aAuthors
+	answer['upvotes'] = aUpvotes
+	answer['comments'] = aComments
 	answers.append(answer)
+
+pprint.pprint(data)
